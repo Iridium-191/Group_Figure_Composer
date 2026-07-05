53c64d27f74b0833cecb85ebda80e720c6f9e834
Iridium-191 <54487721+Iridium-191@users.noreply.github.com>
2026-07-06 00:08:30 +0800

    Delete +gfc directory

--- +gfc/isValidFieldName.m
+++
@@ -1,8 +0,0 @@
-function tf = isValidFieldName(name)
-%ISVALIDFIELDNAME True when NAME can be accessed as S.name in MATLAB.
-
-if isstring(name)
-    name = char(name);
-end
-tf = ischar(name) && isvarname(name) && all(name <= char(127));
-end
--- +gfc/panelCells.m
+++
@@ -1,7 +0,0 @@
-function cells = panelCells(panel)
-%PANELCELLS Return [row col] cells covered by one panel spec.
-
-[rr, cc] = ndgrid(panel.row:(panel.row + panel.rowSpan - 1), ...
-                  panel.col:(panel.col + panel.colSpan - 1));
-cells = [rr(:), cc(:)];
-end
